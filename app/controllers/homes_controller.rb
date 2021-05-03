a306dc0093960cc211c4ff028621c6b15481342d
AyamiYokoi <didufd63hff@gmail.com>
2021-05-03 09:22:26 +0000

    チェックシート上6つ完了

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,16 @@
+class HomesController < ApplicationController
+  def books
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  get 'homes/books'
+  get 'homes/index'
+  get 'homes/show'
+  get 'homes/new'
+  get 'homes/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
