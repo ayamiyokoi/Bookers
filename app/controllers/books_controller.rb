41c4ca85a72cf008949083df96d1a90ba433de50
AyamiYokoi <didufd63hff@gmail.com>
2021-05-04 08:46:02 +0000

    editまで実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
- 
+
 
   def index
     @books = Book.all
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @books = Book.all
     @book = Book.find(params[:id])
   end
 
@@ -22,6 +23,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @books = Book.all
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
   private
