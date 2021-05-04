0816ab57d211fc4d2ca267890bfb31775cb76d07
AyamiYokoi <didufd63hff@gmail.com>
2021-05-04 06:59:07 +0000

    show 実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ resources :books
 
 get 'top' => 'homes#top'
 get 'books/new' => 'books#index'
+# get 'books/:id' => 'books#show', as: 'book'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_083159) do
+ActiveRecord::Schema.define(version: 2021_05_04_054642) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
